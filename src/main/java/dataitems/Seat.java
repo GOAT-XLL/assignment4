300536cdd55d801f7f1248fde554320e55e18524
Xiaoran Li <li.xiaor@husky.neu.edu>
2020-02-17 22:07:48 -0500

    +add seat.java ticket.java

---
+++ src/main/java/dataitems/Seat.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataitems;
+
+/**
+ *
+ * @author Xiaoran Li
+ */
+public class Seat {
+    private int x;
+    private int y;
+    private boolean z;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isZ() {
+        return z;
+    }
+
+    public void setZ(boolean z) {
+        this.z = z;
+    }
+    
+}
